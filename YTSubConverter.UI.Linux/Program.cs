bda8b04ec24d49d3224b3ef0376a26ed094c4080
s0hv <s0hvaperuna@gachimuchi.men>
2026-07-26 17:08:39 +0300

    Defer GUI initialization to allow running on headless Linux environments

--- YTSubConverter.UI.Linux/Program.cs
+++ YTSubConverter.UI.Linux/Program.cs
@@ -7,14 +7,16 @@ namespace YTSubConverter.UI.Linux
     {
         public static void Main(string[] args)
         {
-            Application.Init("ytsubconverter", ref args);
             if (args.Length > 0)
             {
                 using GtkTextMeasurer textMeasurer = new();
                 CommandLineHandler.Handle(args, textMeasurer);
                 return;
             }
-            
+
+            // Initialize application only after checking checking if the application is run as a command line app.
+            // Useful in headless environments, e.g. VMs.
+            Application.Init("ytsubconverter", ref args);
             using var window = new MainWindow();
             window.Show();
             Application.Run();
